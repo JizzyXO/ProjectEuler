b2381c6ffbb2774a3fd4b8a6b6dcd9404fd12046
DasAdErt <78647864+DasAdErt@users.noreply.github.com>
2022-11-25 14:47:51 +0300

    Update Main.java

--- ProjectEuler/src/Main.java
+++ ProjectEuler/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("===============================Проект Эйлера================================");
         System.out.println("Задание 3");
-        TaskNumber3.taskNumber3();
+        TaskNumberThree.taskNumberThree();
     }
 }
 //Ткачёв и Катёлкин
